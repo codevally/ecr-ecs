85668e5e4d3992e9f1feccc47faef54717d19538
Roshan Pradeep <codevally@gmail.com>
2018-09-20 06:03:42 +0000

    Temp

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,3 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "ecs-ecr-tfstate"
-    key = "ecs.tfstate"
-    region = "${var.aws_region}"
-  }
-}
